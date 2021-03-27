6e455787312a128979763c5c0316687053529dd1
Nuri <nkorganci@gmail.com>
2021-03-26 21:57:21 -0400

    version 2

--- src/Git/Git.java
+++ src/Git/Git.java
@@ -1,6 +1,33 @@
 package Git;
 /*
-1-git init-->staging area-->Initialized empty Git repository
+QUESTION:
+hOW CAN WE FIND FILDER GIT
+Intellij automatically updated???
+
+From computer to Github
+
+1- git init-->staging area-->Initialized empty Git repository
+2- git add . --> file was sent to stage area--> (same meaning     git add README.md)
+3- git commit -m "Mesaj"--> stored at local resprotory
+4- git remote add origin https://github.com/nkorganci/winterjava.git --> ready to go to path/address
+5- git push -u origin main
+
+Github to Computer
+
+1-git fetch
+2-git merge
+3-git pull
+4- Nuri branch is created
+5- Nuri branch is created fgbfgf
+
+
+
+4- git log
+5- git branch-->decide/change name of
+
+
+
+
  */
 
 public class Git {
