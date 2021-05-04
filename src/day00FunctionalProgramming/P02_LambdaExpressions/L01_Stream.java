49e6096679becb980379c132c6f9775184421401
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-05-03 20:27:33 -0400

    sdf

--- src/day00FunctionalProgramming/P02_LambdaExpressions/L01_Stream.java
+++ src/day00FunctionalProgramming/P02_LambdaExpressions/L01_Stream.java
@@ -2,7 +2,7 @@ package day00FunctionalProgramming.P02_LambdaExpressions;
 
 public class L01_Stream {
     public static void main(String[] args) {
-        int a=2;//added  ggssdfd
+        int a=2;//added sd ggssdfd
     }
 
 }
