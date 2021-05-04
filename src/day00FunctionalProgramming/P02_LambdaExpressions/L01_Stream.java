a3810a62534210a640980984170337f02e7a360e
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-05-03 20:22:08 -0400

    aa

--- src/day00FunctionalProgramming/P02_LambdaExpressions/L01_Stream.java
+++ src/day00FunctionalProgramming/P02_LambdaExpressions/L01_Stream.java
@@ -1,5 +1,8 @@
 package day00FunctionalProgramming.P02_LambdaExpressions;
 
 public class L01_Stream {
+    public static void main(String[] args) {
+        int a=2;//added  gg
+    }
 
 }
---
+++ src/day00JavaPracticeGroup/W01_.java
@@ -0,0 +1,19 @@
+package day00JavaPracticeGroup;
+
+public class W01_ {
+
+    /*
+* Review "format print" with GroceryList program that's done in Office Hours
+
+Q-1) Write a program that prompts the user to enter numbers. Put them in a list.
+ Then prints each integer with their occurrences.(Use Lambda)
+
+Q-2) Majority element in an array. You are given an array which has numbers inside.
+ You find the element that occurs more than half of the array.
+
+If there is no such element return 0. Suppose the array doesn't contain 0.
+For example if the array is {1,1,2,3} than your program should return 0
+Because the occurrence of "1" is not greater than 2 which is half of the array length. (2>2) is false
+If the array is {1,2,2,2,3} than your program should return 2
+     */
+}
