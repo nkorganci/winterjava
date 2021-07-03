2a6d08a7449aea569b8139baf4ac5809629b297a
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-03 18:03:49 -0500

    6 03

--- src/A01codingbat/warmup01.java
+++ src/A01codingbat/warmup01.java
@@ -3,8 +3,15 @@ package A01codingbat;
 public class warmup01 {
     public static void main(String[] args) {
 
+        ex("abcxx");
     }
-    public static String ex(String str, int n){
 
+    public static boolean ex(String str) {
+
+        String result="";
+        for(int i=0; i<str.length(); i=i+2){
+            result=result+str.charAt(i);
+        }
+        return result;
 
 }
