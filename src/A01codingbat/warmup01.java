1f3ca972ec81d3a56651dd8bf1e6beb63e10080b
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-04 06:17:19 -0500

    7 4

--- src/A01codingbat/warmup01.java
+++ src/A01codingbat/warmup01.java
@@ -3,14 +3,17 @@ package A01codingbat;
 public class warmup01 {
     public static void main(String[] args) {
 
-        ex("abc");
+        int [] a ={2,6,9,9};
+        System.out.println(ex(a));
     }
 
-    public static boolean ex(String str) {
-        String result="";
-        for(int i=0;i<str.length();i++){
-            result=result+str.substring(0,i);
-        }
-        return result;
+    public static int ex(int [] nums) {
 
+    int counter=0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]==6&&(nums[i+1]==6||nums[i+1]==7){
+            counter++;
+        }
+    }
+    return counter;
 }
