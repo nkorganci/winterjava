7fcb515d5ec2073a5e831845fa4255c28938279d
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-09 11:05:13 -0500

    7 9

--- src/A01codingbat/warmup01.java
+++ src/A01codingbat/warmup01.java
@@ -4,11 +4,21 @@ public class warmup01 {
     public static void main(String[] args) {
 
 
-        System.out.println(ex("a"));
+        System.out.println(ex("1234"));
 
     }
 
-    public static String ex(String a, String b) {
+    public static boolean ex(String str, int n) {
+
+        if (str.length() < 3) {
+            return false;
+        } else if (str.length() > 3 && (str.substring(0, 3).equals("bad") || str.substring(1, 4).equals("bad"))){
+            return true;
+        }else {
+            return false;
+
+        }
 
 
-}
+    }
+
