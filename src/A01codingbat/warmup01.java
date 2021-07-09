5442c034fbea9588d9fc8f01823ba4e2ba2c5cf8
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-09 18:26:00 -0500

    hi

--- src/A01codingbat/warmup01.java
+++ src/A01codingbat/warmup01.java
@@ -4,12 +4,21 @@ public class warmup01 {
     public static void main(String[] args) {
 
 
-        System.out.println(ex("re"));
+        System.out.println(ex("re", "eesdf"));
 
     }
 
-    public static boolean ex(String a, String b) {
+    public static String ex(String a, String b) {
+        String res="";
+        if(a.length()==b.length()){
+            res= a+b;
+        }else if(a.length()>b.length()){
+            res= a.substring(a.length()-b.length())+b;
+        }else if(a.length()<b.length()){
+            res= a+b.substring(b.length()-a.length()) ;
+        }
+        return res;
+    }
 
-        if(a.length())
 }
 
