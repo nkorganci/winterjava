b76d92e3296a9b98deb2a23f5812bfbf25857ce4
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-11 10:35:18 -0500

    m

--- src/A01codingbat/CodingBat.java
+++ src/A01codingbat/CodingBat.java
@@ -5,25 +5,17 @@ public class CodingBat {
 
         int [] a = {1,2,6};
         System.out.println(coding(a));
-        coding1(a);
-    }
-
-    public static int[] coding(int[] a){
-        int [] c= new int[2];
-
-        if(a.length==1){
-            c[0]=a[0];
-            c[1]=a[0];
-        }else {
-                c[0]=a[0];
-                c[1]=a[a.length-1];
-    }
-        return c;
-    public static void coding1(int [] nums){
-
-        System.out.println(nums[nums.length-1]);
-
 
     }
 
-}
+    public static int[] coding(int[] a, int[] b){
+
+   int []  sum = new int[4];
+   for(int i=0;i<a.length;i++){
+       sum[i]=a[i];
+   }
+   for(int j=0;j<b.length;j++){
+       sum[a.length+j]=b[j];
+   }
+   return sum;
+
