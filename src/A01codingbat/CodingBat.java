666338eb1d498261d8c6b6b3c676f76ccb1cfc57
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-18 09:16:07 -0500

    m

--- src/A01codingbat/CodingBat.java
+++ src/A01codingbat/CodingBat.java
@@ -1,23 +1,20 @@
 package A01codingbat;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 public class CodingBat {
 
+    public boolean xyzThere(String str) {
 
-    public int makeChocolate(int small, int big, int goal) {
-
-        if(small+5*big<goal){
-            return -1;
-        }else if(small<big%5){
-            return -1;
-        }else {
-            if(goal<10){
-                return goal%5;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='b'&&str.charAt(i+2)=='b'){
+                return true;
             }
-        }return goal-big*5;
+        }
+        return false;
 
-    }
+}
 
 
 
