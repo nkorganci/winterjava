7247ffd84e7ae6433fc186e1577409394a3ca8a8
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-21 21:53:53 -0500

    m

--- src/A01codingbat/CodingBat.java
+++ src/A01codingbat/CodingBat.java
@@ -5,14 +5,11 @@ import java.util.Locale;
 
 public class CodingBat {
 
-    public boolean xyzThere(String str) {
 
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)=='b'&&str.charAt(i+2)=='b'){
-                return true;
-            }
-        }
-        return false;
+    public String doubleChar(String str) {
+
+        int count=0;
+
 
 }
 
