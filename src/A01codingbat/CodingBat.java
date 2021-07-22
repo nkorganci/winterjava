777dce5ea634ec843ce9fcfd0af1b0616623a3cd
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-22 07:36:38 -0500

    m

--- src/A01codingbat/CodingBat.java
+++ src/A01codingbat/CodingBat.java
@@ -5,13 +5,28 @@ import java.util.Locale;
 
 public class CodingBat {
 
-
-    public String doubleChar(String str) {
-
-        int count=0;
+    public static void main(String[] args) {
 
 
-}
+    }
+    public String mixString(String a, String b) {
+
+        int min=Math.min(a.length(),b.length());
+
+        int x=0;
+        String total="";
+
+        while (x<min){
+            String aSub=a.substring(x,x+1);
+            String bSub=b.substring(x,x+1);
+            total+=aSub+bSub;
+            x++;
+        }
+        return total+a.substring(x) + b.substring(x);
+    }
+
+
+    }
 
 
 
