24837c6adab491e81dbed84784a3d43f4cd55f9f
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-27 19:15:16 -0500

    string3

--- src/A01codingbat/CodingBat.java
+++ src/A01codingbat/CodingBat.java
@@ -1,28 +1,30 @@
 package A01codingbat;
 
-import java.util.Arrays;
-import java.util.Locale;
-
 public class CodingBat {
-
     public static void main(String[] args) {
 
-
     }
-    public String getSandwich(String str) {
 
+    public int countYZ(String str) {
 
-        for (int i = 1; i < str.length()-1; i++) {
-            if(str.charAt(i)=='*'&&str.charAt(i-1)==str.charAt(i+1)){
-                return true;
+        int length = str.length();
+        int count = 0;
+        str=str.toLowerCase();
+
+        for (int i=0;i<length;i++){
+            if(str.charAt(i)=='y' || str.charAt(i)=='z'){
+                if(i<length-1 &&!Character.isLetter(str.charAt(i+1))){
+                    count++;
+                }else if(i==length-1){
+                    count++;
+                }
             }
         }
-        return false;
 
 
+        return count;
+
+    }
 
 
 }
-
-
-
