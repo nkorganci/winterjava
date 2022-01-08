890b1e0030c58945a793aa0a6f3113f2fb5941c3
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2022-01-08 16:34:11 -0600

    m

--- src/day31a_Collections/A.java
+++ src/day31a_Collections/A.java
@@ -8,16 +8,13 @@ public class A {
 
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        List<String> list = new LinkedList<>();
         list.add("D");
         list.add("B");
         list.add("C");
-
         System.out.println(list);
-
-   Collections.sort(list);
+        Collections.sort(list);
         System.out.println(list);
-
     }
 }
 
