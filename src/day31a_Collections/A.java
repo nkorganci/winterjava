dc29c1466992e11e070d383c0c825dd47c28bfcf
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2022-01-09 10:29:13 -0600

    m

--- src/day31a_Collections/A.java
+++ src/day31a_Collections/A.java
@@ -2,20 +2,45 @@ package day31a_Collections;
 
 import jdk.swing.interop.SwingInterOpUtils;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class A {
 
     public static void main(String[] args) {
 
-        List<String> list = new LinkedList<>();
-        list.add("D");
-        list.add("B");
-        list.add("C");
-        System.out.println(list);
-        Collections.sort(list);
-        System.out.println(list);
+    }
+}
+
+class B {
+    public static void main(String[] args) {
+
+
     }
 }
 
 
+class Base {
+    private static void display() {
+        System.out.println("Static or class method from Base");
+    }
+
+    public void print() {
+        System.out.println("Non-static or instance method from Base");
+    }
+
+    class Derived extends Base {
+        private static void display() {
+            System.out.println("Static or class method from Derived");
+        }
+
+        public void print() {
+            System.out.println("Non-static or instance method from Derived");
+        }
+
+        public class test {
+            public static void main(String args[]) {
+                Base obj = new Derived();
+                obj1.display();
+                obj1.print();
+            }
+        }
