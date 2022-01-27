3634b5b4a077c539e6ca560107e66296229d9a4e
Nuri Korganci <nkorganci@gmail.com>
2022-01-26 22:54:11 -0600

    Modifier

---
+++ src/day21accessmodifierstringbuilder_ReferenceForPractice/first/First.java
@@ -0,0 +1,10 @@
+package day21accessmodifierstringbuilder_ReferenceForPractice.first;
+
+public class First {
+
+    String str;
+    First(String str){
+        this.str=str;
+    }
+
+}
---
+++ src/day21accessmodifierstringbuilder_ReferenceForPractice/first/Main.java
@@ -0,0 +1,9 @@
+package day21accessmodifierstringbuilder_ReferenceForPractice.first;
+
+public class Main {
+
+    public static void main(String[] args) {
+        First a = new First("Hello World");
+        System.out.println(a.str);
+    }
+}
