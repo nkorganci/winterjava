564dee74cbede0806afcd895c94ad2ce82473b87
Nuri Korganci <nkorganci@gmail.com>
2022-02-02 15:54:07 -0600

    m

---
+++ src/a01codingpractice/Solution.java
@@ -0,0 +1,25 @@
+package a01codingpractice;
+
+import java.util.Scanner;
+
+public class Solution {
+    static Scanner scan = new Scanner(System.in);
+    static int B = scan.nextInt();
+    static int H = scan.nextInt();
+    static boolean flag = true;
+
+    static {
+        try {
+            if (B <= 0 || H <= 0) {
+                flag = flag;
+                throw new Exception("Breadth and height must be positive");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        public static void main(String[] args){
+
+        }
+    }
+}
