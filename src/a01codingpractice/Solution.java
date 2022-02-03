67ed103fc56339e7e2c48ef64e1c49b78c9bd27f
Nuri Korganci <nkorganci@gmail.com>
2022-02-03 08:54:10 -0600

    final

--- src/a01codingpractice/Solution.java
+++ src/a01codingpractice/Solution.java
@@ -3,23 +3,35 @@ package a01codingpractice;
 import java.util.Scanner;
 
 public class Solution {
+
+    //Write your code here
     static Scanner scan = new Scanner(System.in);
-    static int B = scan.nextInt();
-    static int H = scan.nextInt();
-    static boolean flag = true;
+    public static int B=scan.nextInt();
+    public static int H=scan.nextInt();
+    public static boolean flag=true;
 
     static {
-        try {
-            if (B <= 0 || H <= 0) {
-                flag = flag;
-                throw new Exception("Breadth and height must be positive");
+        try{
+            if(B<=0 || H<=0){
+                flag=false;
+                throw new Exception("java.lang.Exception: Breadth and height must be positive");
+            }else {
+                flag=true;
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        public static void main(String[] args){
+        }catch(Exception e){
+            System.out.println(e.getMessage());
 
         }
+
     }
-}
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
+
