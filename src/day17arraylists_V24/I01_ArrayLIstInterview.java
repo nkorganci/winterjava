55fb88c9496d4d42ea7cc3a0a328a0a4872ed711
Nuri Korganci <nkorganci@gmail.com>
2022-03-24 17:16:10 -0500

    m

--- src/day17arraylists_V24/I01_ArrayLIstInterview.java
+++ src/day17arraylists_V24/I01_ArrayLIstInterview.java
@@ -1,9 +1,7 @@
 package day17arraylists_V24;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.net.PortUnreachableException;
+import java.util.*;
 
 public class I01_ArrayLIstInterview {
     public static void main(String[] args) {
@@ -11,19 +9,26 @@ public class I01_ArrayLIstInterview {
         l.add(2);
         l.add(1);
         l.add(1);
-        l.add(2);
-        removeDublicates(l);
-        System.out.println(l);
+        l.add(3);
+
+        //removeDublicates(l);
+        System.out.println(reverseArray(l));
+
 
     }
     public static List<Integer> removeDublicates(List<Integer> list){
-
         Set<Integer> removedDublicates = new LinkedHashSet<>(list);
                 list.clear();
-
                 list.addAll(removedDublicates);
-
                 return list;
 
+    }
+    public static List<Integer> reverseArray(List<Integer> list1){
+
+   Collections.reverse(list1);
+        return list1;
+    }
+    public static List<Integer> reverseArray1(List<Integer> list){
+        return list;
     }
 }
