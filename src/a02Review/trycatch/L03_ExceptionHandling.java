395c118a77f7b9a114474d0bb6078c20d73924a0
Nuri Korganci <nkorganci@gmail.com>
2022-04-10 09:52:08 -0500

    m

--- src/a02Review/trycatch/L03_ExceptionHandling.java
+++ src/a02Review/trycatch/L03_ExceptionHandling.java
@@ -1,5 +1,7 @@
 package a02Review.trycatch;
 
+import java.util.InputMismatchException;
+import java.util.MissingFormatArgumentException;
 import java.util.Scanner;
 
 public class L03_ExceptionHandling {
@@ -7,43 +9,20 @@ public class L03_ExceptionHandling {
 
 class Exception{
     public static void main(String[] args) {
-        int a = 2/0; // I did not print but I did calculation which is exception.
-    }
-}
+        Scanner scan =  new Scanner(System.in);
 
-class MyCalculator{
-    public static void main(String[] args) {
+        try{
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println(a/b);
+        }catch (ArithmeticException e){
 
-    }
+                System.out.println("hmm");
+            }catch (MissingFormatArgumentException e){
 
-    public static int power(int n, int p) throws java.lang.Exception {
-
-        if(n<0 || p<0){
-            throw new java.lang.Exception("N can not be negative..");
-        }else if(n==0 && p==0){
-            throw new java.lang.Exception("Both of them can not be zero...");
-        }else {
-            return (int)Math.pow(n,p);
         }
-
-
-    }
-}
-
-class Solution{
-   static MyCalculator myCalculator = new MyCalculator();
-    static Scanner in = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        while (in.hasNextInt()){
-            int n = in.nextInt();
-            int p = in.nextInt();
-
-            try {
-                System.out.println(myCalculator.power(n,p));
-            }catch (java.lang.Exception e){
-                System.out.println(e);
             }
-        }
-    }
+
+
 }
+
