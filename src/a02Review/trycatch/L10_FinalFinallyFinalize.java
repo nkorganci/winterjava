7cce2ccbc8f05cab0432a7fdf610feebecb62824
Nuri Korganci <nkorganci@gmail.com>
2022-04-10 19:10:12 -0500

    m

--- src/a02Review/trycatch/L10_FinalFinallyFinalize.java
+++ src/a02Review/trycatch/L10_FinalFinallyFinalize.java
@@ -1,15 +1,33 @@
 package a02Review.trycatch;
 
+import java.io.IOException;
+
 public class L10_FinalFinallyFinalize {
 }
 
-class E{
-    public static void main(String[] args) {
-        E obj = new E();
+class UserException extends Exception{
+    public UserException(String str){
+        super(str);
+    }
+}
 
-        System.out.println("Hashcode " + obj.hashCode());
-        obj = null;
-        System.gc();
-        System.out.println("end");
+class TT{
+    static void validate(int age) throws UserException{
+        if(age<18){
+            throw new UserException("age not valid");
+        }else {
+            System.out.println("you can vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            validate(13);
+        }catch (UserException e){
+            System.out.println("caught");
+            System.out.println("Exception occured "+ e);
+        }
+
+        System.out.println("rest");
     }
 }
