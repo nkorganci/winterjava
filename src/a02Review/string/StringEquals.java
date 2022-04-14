df01de3854b05e6d98948bb93491e92cb1b4ee98
Nuri Korganci <nkorganci@gmail.com>
2022-04-14 02:56:05 -0500

    m

---
+++ src/a02Review/string/StringEquals.java
@@ -0,0 +1,9 @@
+package a02Review.string;
+
+public class StringEquals {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = new String(a);
+        System.out.println(a==b);
+    }
+}
