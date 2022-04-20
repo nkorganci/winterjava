7c3ccd0593188ecbc65141b27af561fe0ebb2b9c
Nuri Korganci <nkorganci@gmail.com>
2022-04-20 13:53:34 -0500

    m

---
+++ src/a04geeksforgeeks/Set1.java
@@ -0,0 +1,9 @@
+package a04geeksforgeeks;
+public class Set1 {
+    public static void main(String[] args) {
+        for (int i = 0; i<2 ; i++) {
+            System.out.println("hi");
+            break;
+        }
+    }
+}
