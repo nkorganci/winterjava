3f2ec15ec2ddba0572947bd868381875f3182049
Nuri Korganci <nkorganci@gmail.com>
2022-04-26 17:16:12 -0500

    m

--- src/a02Review/garbagecollector/SampleTest.java
+++ src/a02Review/garbagecollector/SampleTest.java
@@ -4,36 +4,15 @@ public class SampleTest {
 }
 
 
- class Base
-{
-    private int data;
+class A{
+    public static void main(String[] args) {
 
-    public Base()
-    {
-        data = 5;
-    }
-
-    public int getData()
-    {
-        return this.data;
-    }
-}
-
-class Derived extends Base
-{
-    private int data;
-    public Derived()
-    {
-        data = 6;
-    }
-    public int getData()
-    {
-        return data;
-    }
-
-    public static void main(String[] args)
-    {
-        Derived myData = new Derived();
-        System.out.println(myData.getData());
+        int a = 2;
+        try {
+            System.out.println(a/0);
+            throw  new ArithmeticException("no zeor");
+        }catch (IndexOutOfBoundsException e){
+            System.out.println(e);;
+        }
     }
 }
