8f8a2e43e3d298a0776129f8231db26c89f77d39
Nuri Korganci <nkorganci@gmail.com>
2022-05-03 16:28:15 -0500

    m

--- src/a02Review/garbagecollector/SampleTest.java
+++ src/a02Review/garbagecollector/SampleTest.java
@@ -1,25 +1,14 @@
 package a02Review.garbagecollector;
-
-import jdk.swing.interop.SwingInterOpUtils;
-
-import java.io.IOException;
-
 public class SampleTest {
 }
 
 class A{
-    public  void run(){
-        System.out.println("running in A");
-    }
-}
-
-class B extends A{
-    public  void run(){
-        System.out.println("Running in B");
-    }
-
     public static void main(String[] args) {
-        B obj = new B();
-        obj.run();
+        int a=9;
+        try {
+            System.out.println(a/0);
+        }finally {
+            System.out.println("final");
+        }
     }
 }
