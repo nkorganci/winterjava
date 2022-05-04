546b79a67cd8411ddfcf7cdf685bbe7168494214
Nuri Korganci <nkorganci@gmail.com>
2022-05-04 17:47:04 -0500

    m

---
+++ src/a02Review/collections1/Sample.java
@@ -0,0 +1,9 @@
+package a02Review.collections1;
+
+public class Sample {
+    public static void main(String[] args) {
+      String a = new String("Hi");
+      String b = "Hi";
+        System.out.println(a.equals(b));
+    }
+}
