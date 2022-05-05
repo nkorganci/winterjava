a05896cffb00862ff6f5eaeb2a3ed4c3bddea444
Nuri Korganci <nkorganci@gmail.com>
2022-05-05 15:48:03 -0500

    m

--- src/a02Review/collections1/Sample.java
+++ src/a02Review/collections1/Sample.java
@@ -1,9 +1,14 @@
 package a02Review.collections1;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Sample {
-    public static void main(String[] args) {
-      String a = new String("Hi");
-      String b = "Hi";
-        System.out.println(a.equals(b));
-    }
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("notExist.txt");
+        FileInputStream stream = new FileInputStream(file);
+
+        }
 }
