408025219efa861c04520eb0f1fa79c4692bd38f
Nuri Korganci <nkorganci@gmail.com>
2022-05-17 13:46:08 -0500

    m

--- src/a02Review/trycatch/Example.java
+++ src/a02Review/trycatch/Example.java
@@ -1,12 +1,22 @@
 package a02Review.trycatch;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Example {
 
-    public static void main(String[] args) {
-        int a = 2;
-        for (int i = 0; i < 10; i+=a) {
-            System.out.print(i -a + " ");
-            a++;
+    public static void main(String[] args)  {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" yasini yaz: ");
+        int yasiniz = scan.nextInt();
+        System.out.println(" boyunu yaz: ");
+        int boyunuz = scan.nextInt();
+        System.out.println("Average : " +  (yasiniz+boyunuz)/2);
+
+
+
         }
     }
-}
