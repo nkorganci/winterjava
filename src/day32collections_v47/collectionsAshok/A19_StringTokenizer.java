df55fcaf45aac2cd08f2ec98c244afbf5088ccf4
Nuri Korganci <nkorganci@gmail.com>
2023-01-07 09:17:05 -0600

    m

---
+++ src/day32collections_v47/collectionsAshok/A19_StringTokenizer.java
@@ -0,0 +1,20 @@
+package day32collections_v47.collectionsAshok;
+
+import java.util.StringTokenizer;
+
+public class A19_StringTokenizer {
+    public static void main(String[] args) {
+        String str = "a b c";
+        StringTokenizer t = new StringTokenizer(str);
+
+        System.out.println(t);
+
+        // You can print all
+        while (t.hasMoreTokens()){
+            System.out.println(t.nextToken());
+        }
+
+        // You can prin
+        System.out.println("Next Token is : " + t.nextToken());
+    }
+}
---
+++ src/day36lambda/lambdaaskhok/A01_RegularInterface.java
@@ -0,0 +1,19 @@
+package day36lambda.lambdaaskhok;
+
+public interface A01_RegularInterface {
+    public void run();
+}
+
+class A implements A01_RegularInterface{
+
+    @Override
+    public void run() {
+        System.out.println("run ");
+    }
+
+    public static void main(String[] args) {
+
+        A obj = new A();
+        obj.run();
+    }
+}
