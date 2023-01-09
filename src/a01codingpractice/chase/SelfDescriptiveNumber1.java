5b957878cc1f0bba379eb1c87b56f2615778a62b
Nuri Korganci <nkorganci@gmail.com>
2023-01-08 21:35:39 -0600

    m

--- src/a01codingpractice/chase/SelfDescriptiveNumber1.java
+++ src/a01codingpractice/chase/SelfDescriptiveNumber1.java
@@ -5,42 +5,40 @@ public class SelfDescriptiveNumber1 {
     public static void main(String[] args) {
 
         System.out.println(isSelfDescriptiveNumber(2020));
-
-
     }
 
     public static boolean isSelfDescriptiveNumber(int num) {
-        //Converting integer num to String
-        String s = Integer.toString(num);
 
+        // converting the integer num to string
+        String s = Integer.toString(num);
         for (int i = 0; i < s.length(); i++) {
-            //Extracting each digit one by one from the string
+
+            // Extracting each digit one by one from the string
             String temp_char = s.charAt(i) + "";
 
-            //Converting the string(digit) into integer
-            //b variable stores the digit present at index "i"
+            // converting the string (digit) into integer
+            // b variable stores the digit present at index 'i'
             int b = Integer.parseInt(temp_char);
 
-            //Counting how many times particular digit occur in  the whole "num"
+            // counting how many times the particular digit
+            // occur in the whole number "num"
             int count = 0;
             for (int j = 0; j < s.length(); j++) {
+                // converting string char to integer
+                int temp = Integer.parseInt(s.charAt(j) + "");
 
-                //Converting string char to integer
-                int temp = Integer.parseInt(s.charAt(j)+"");
-
-                //Checking weather it is equal to the index 'i'
-                //If it is then increment the count.
-                if(temp==i){
+                // checking whether it is equal to the index 'i'
+                // if it is then increment the count .
+                if (temp == i) {
                     count++;
                 }
             }
 
-          //If it is not equal, it returns false
-            if(count!=b){
+            // If it is not equal
+            // it return false .
+            if (count != b)
                 return false;
-            }
         }
-
         return true;
     }
 }
