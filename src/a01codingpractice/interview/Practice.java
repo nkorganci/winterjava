2a604ce5dfb5d1325058ffec2546a5ba017f6323
Nuri Korganci <nkorganci@gmail.com>
2023-01-09 09:03:44 -0600

    m

---
+++ src/a01codingpractice/interview/Practice.java
@@ -0,0 +1,13 @@
+package a01codingpractice.interview;
+
+public class Practice {
+
+    int a;
+    static int c;
+    final static int b;
+    public static void main(String[] args) {
+
+
+
+    }
+}
