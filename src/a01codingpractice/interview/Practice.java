c8ed3d1a4c40954f142062ebe0f4cdf6a6bb2ad9
Nuri Korganci <nkorganci@gmail.com>
2023-01-10 08:58:24 -0600

    m

--- src/a01codingpractice/interview/Practice.java
+++ src/a01codingpractice/interview/Practice.java
@@ -2,9 +2,7 @@ package a01codingpractice.interview;
 
 public class Practice {
 
-    int a;
-    static int c;
-    final static int b;
+
     public static void main(String[] args) {
 
 
---
+++ src/day15arrays_V20/ObjectMethods.java
@@ -0,0 +1,7 @@
+package day15arrays_V20;
+
+public class ObjectMethods {
+    public static void main(String[] args) {
+
+    }
+}
