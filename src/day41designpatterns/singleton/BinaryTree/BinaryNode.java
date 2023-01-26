af82fb6929168e92d7a0f3e74009cdcd37f7ca87
Nuri Korganci <nkorganci@gmail.com>
2023-01-25 23:14:21 -0600

    m

---
+++ src/day41designpatterns/singleton/BinaryTree/BinaryNode.java
@@ -0,0 +1,9 @@
+package day41designpatterns.singleton.BinaryTree;
+
+public class BinaryNode {
+
+    public String value;
+    public BinaryNode left;
+    public BinaryNode right;
+    public int height;
+}
--- src/day41designpatterns/singleton/BinaryTree/Main.java
+++ src/day41designpatterns/singleton/BinaryTree/Main.java
@@ -1,25 +1,39 @@
 package day41designpatterns.singleton.BinaryTree;
 
 public class Main {
-
     public static void main(String[] args) {
-        TreeNode drinks = new TreeNode("Drinks");
-        TreeNode hot = new TreeNode("hot");
-        TreeNode cold = new TreeNode("cold");
-        TreeNode tea = new TreeNode("tea");
-        TreeNode coffee = new TreeNode("coffee");
-        TreeNode water = new TreeNode("water");
-        TreeNode coke = new TreeNode("coke");
+        BinaryTreeLL binaryTreeLL = new BinaryTreeLL();
+        BinaryNode n1 = new BinaryNode();
+        n1.value="n1";
+        BinaryNode n2 = new BinaryNode();
+        n2.value="n2";
+        BinaryNode n3 = new BinaryNode();
+        n3.value="n3";
+        BinaryNode n4 = new BinaryNode();
+        n4.value="n4";
+        BinaryNode n5 = new BinaryNode();
+        n5.value="n5";
+        BinaryNode n6 = new BinaryNode();
+        n6.value="n6";
+        BinaryNode n7 = new BinaryNode();
+        n7.value="n7";
+        BinaryNode n8 = new BinaryNode();
+        n8.value="n8";
+        BinaryNode n9 = new BinaryNode();
+        n9.value="n9";
 
-        drinks.addChild(hot);
-        drinks.addChild(cold);
-        hot.addChild(tea);
-        hot.addChild(coffee);
-        cold.addChild(water);
-        cold.addChild(coke);
+        n1.left=n2;
+        n1.right=n3;
+        n2.left=n4;
+        n2.right=n5;
+        n4.left=n8;
+        n4.right=n9;
 
-        System.out.println(drinks.print(0));
+        n3.left=n6;
+        n3.right=n7;
+
+        binaryTreeLL.root=n1;
+
+        binaryTreeLL.preOrder(binaryTreeLL.root);
     }
-
-
 }
