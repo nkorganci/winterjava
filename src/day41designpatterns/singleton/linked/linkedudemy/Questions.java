e8e2e7fe7c0f6a4c5257813f3031b139b3cedf13
Nuri Korganci <nkorganci@gmail.com>
2023-01-29 23:37:54 -0600

    m

---
+++ src/day41designpatterns/singleton/linked/linkedudemy/Questions.java
@@ -0,0 +1,22 @@
+package day41designpatterns.singleton.linked.linkedudemy;
+
+import java.util.HashSet;
+
+public class Questions {
+    void deleteDups(LinkedList1 linkedList1) {
+        HashSet<Integer> hs = new HashSet<>();
+        Node1 current = linkedList1.head;
+        Node1 prev = null;
+        while (current != null) {
+            int curval = current.value;
+            if (hs.contains(curval)) {
+                prev.next = current.next;
+                linkedList1.size--;
+            } else {
+                hs.add(curval);
+                prev = current;
+            }
+            current = current.next;
+        }
+    }
+}
