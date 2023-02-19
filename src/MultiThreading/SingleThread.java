f9f49bbcb10d696c9017cd095daafc3a0ead21d3
Nuri Korganci <nkorganci@gmail.com>
2023-02-19 12:19:43 -0600

    m

--- src/MultiThreading/SingleThread.java
+++ src/MultiThreading/SingleThread.java
@@ -1,11 +1,29 @@
 package MultiThreading;
 
-public class SingleThread {
+import javax.sql.rowset.serial.SQLInputImpl;
+
+public class SingleThread extends Thread{
+
+    @Override
+    public void run(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println(Thread.currentThread());
-        System.out.println(Thread.currentThread());
+
+        SingleThread obj1 = new SingleThread();
+        SingleThread obj2 = new SingleThread();
+
+        obj1.start();
+        obj2.start();
+
+        System.out.println(obj1.getName());
     }
 }
