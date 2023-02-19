88317a0f249c3d7133fbe2ea733d03f7966a61f7
Nuri Korganci <nkorganci@gmail.com>
2023-02-18 23:16:06 -0600

    m

---
+++ src/day41designpatterns/datastructure/binarytree/BinaryTreePreorder.java
@@ -0,0 +1,18 @@
+package day41designpatterns.datastructure.binarytree;
+
+public class BinaryTreePreorder {
+
+
+}
+
+class BinaryNode{
+    int data;
+    BinaryNode left;
+    BinaryNode right;
+
+    public BinaryNode(int data, BinaryNode left, BinaryNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
